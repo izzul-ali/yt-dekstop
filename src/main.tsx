a647ccd8157243e21d9a8d8caeb4312a283eb8ef
izzul-ali <izzulmaali3@gmail.com>
2022-11-04 21:30:52 +0700

    add fallback component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/css/index.css'
+import Fallback from './components/Fallback'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Suspense>
+    <Suspense fallback={<Fallback />}>
       <App />
     </Suspense>
   </React.StrictMode>
